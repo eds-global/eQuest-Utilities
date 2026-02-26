f348be80a23ca5b3a1d970ec81801566c5f8d353
Kumaramaon01 <rajeev@edsglobal.com>
2026-02-26 12:47:28 +0530

    added latest updates with dates on each sections

--- SIM_Parser/sim_parserv01.py
+++ SIM_Parser/sim_parserv01.py
@@ -11,31 +11,23 @@ def get_report_and_save(report_function, sim_path, file_suffix, title_container,
 
     # session key (safe per container)
     key = f"status_messages_{id(status_container)}"
-
     # init state
     if key not in st.session_state:
         st.session_state[key] = []
-
     try:
         report = report_function(sim_path)
         file_name = os.path.splitext(os.path.basename(sim_path))[0]
-
         with tempfile.NamedTemporaryFile(delete=False, suffix=".csv") as temp_file:
             report.to_csv(temp_file.name, index=False)
             temp_file_path = temp_file.name
-
         # add success badge
-        st.session_state[key].append(
-            f"<span style='color:green; font-weight:600; margin-right:14px;'>✅ {file_suffix}</span>"
-        )
-
+        st.session_state[key].append(f"<span style='color:green; font-weight:600; font-size:13px; margin-right:14px;'>✅ {file_suffix}</span>")
     except Exception:
         # add error badge
-        st.session_state[key].append(f"<span style='color:red; font-weight:600; margin-right:14px;'>❌ {file_suffix}</span>")
+        st.session_state[key].append(f"<span style='color:red; font-weight:600; font-size:13px; margin-right:14px;'>❌ {file_suffix}</span>")
         temp_file_path = None
-
     # -------- UI Render --------
-    title_container.markdown("<b style='color:green; font-weight:600; font-size:1200;'>SIM Reports:</b>",unsafe_allow_html=True)
+    title_container.markdown("<b style='color:black; font-weight:600; font-size:1200;'>SIM Reports:</b>",unsafe_allow_html=True)
 
     status_container.markdown(
         f"<div style='white-space:nowrap; overflow-x:auto; padding-top:4px;'>"
@@ -58,12 +50,12 @@ def main(uploaded_file):
 
         report_functions = [
             (ls_c.get_LSC_report, 'LSC.csv', 'LS-C'),
-            (ls_c.get_LSC_losses_report, 'LSC_LOSS.csv', 'LS-C_LOSS'),
+            (ls_c.get_LSC_losses_report, 'LSC_LOSS.csv', 'LS-C LOSS'),
             (lv_d.get_LVD_report, 'LVD.csv', 'LV-D'),
-            (lvd_summary.get_LVD_Summary_report, 'LVD_Summary.csv', 'LV-D_Summary'),
+            (lvd_summary.get_LVD_Summary_report, 'LVD_Summary.csv', 'LV-D Summary'),
             (pv_a.get_PVA_report, 'PVA.csv', 'PV-A'),
             (sv_a.get_SVA_report, 'SVA.csv', 'SV-A'),
-            (sva_zone.get_SVA_Zone_report, 'SVA_Zone.csv', 'SV-A_Zone'),
+            (sva_zone.get_SVA_Zone_report, 'SVA_Zone.csv', 'SV-A Zone'),
             (beps.get_BEPS_report, 'BEPS.csv', 'BEPS'),
             (bepu.get_BEPU_report, 'BEPU.csv', 'BEPU'),
             (lv_b.get_LVB_report, 'LVB.csv', 'LV-B'),
@@ -80,7 +72,7 @@ def main(uploaded_file):
                 download_files.append((file_name, file_path))
 
         if download_files:
-            st.markdown(f"<br><span style='color:green;'>✅ SIM Parsed Successfully!!</span>", unsafe_allow_html=True)
+            st.markdown(f"<br><span style='color:green;'></span>", unsafe_allow_html=True)
 
             # Create a zip file containing all generated reports
             with tempfile.NamedTemporaryFile(delete=False, suffix=".zip") as temp_zip:
